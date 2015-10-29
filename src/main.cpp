2df829c2cedb95c9da1c613aec7ddae5afcaa307
Darenn Keller <darenn.keller@etu.univ-orleans.fr>
2015-10-29 11:59:56 +0100

    Adding a framerate of 60 FPS in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <SDL2/SDL.h>
 #include "Game.h"
 
+const int FPS = 60;
+const int DELAY_TIME = 1000.0f / FPS;
+
 int main(int argc, char* args[]) 
 {
+  Uint32 frameStart, frameTime;
   TheGame::Instance()->init("Chapter 1 : setting up SDL",
 	       SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 	       640, 480,
 	       false);
   while (TheGame::Instance()->running())
     { 
+      frameStart = SDL_GetTicks();
       TheGame::Instance()->handleEvents();
       TheGame::Instance()->update();
       TheGame::Instance()->render();
-      SDL_Delay(20);
+      frameTime = SDL_GetTicks() - frameStart;
+      if(frameTime < DELAY_TIME)
+	{
+	  SDL_Delay((int)(DELAY_TIME - frameTime));
+	}
     }
 
   TheGame::Instance()->clean();
