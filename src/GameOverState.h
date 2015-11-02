1486430d84570383137235e1972c4ae71d892faa
Darenn Keller <darenn.keller@etu.univ-orleans.fr>
2015-11-02 10:57:28 +0100

    adding a game over state.

---
+++ src/GameOverState.h
@@ -0,0 +1,24 @@
+#ifndef __GAMEOVERSTATE__
+#define __GAMEOVERSTATE__
+
+#include "GameState.h"
+#include <vector>
+#include <string>
+#include "GameObject.h"
+
+class GameOverState : public GameState
+{
+public:
+  virtual void update();
+  virtual void render();
+  virtual bool onEnter();
+  virtual bool onExit();
+  virtual std::string getStateID() const {return s_gameOverID;}
+private:
+  static void s_gameOverToMain();
+  static void s_restartPlay();
+  static const std::string s_gameOverID;
+  std::vector<GameObject*> m_gameObjects;
+};
+
+#endif
