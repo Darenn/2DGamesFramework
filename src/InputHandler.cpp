f794c642de30c66fe497a258aa8686deda09da05
Darenn Keller <keller.darenn@gmail.com>
2015-11-06 14:22:14 +0100

    Optimize and doc InputHandler class

--- src/InputHandler.cpp
+++ src/InputHandler.cpp
@@ -1,29 +1,24 @@
 #include "InputHandler.h"
+#include <iostream>
 #include "Game.h"
-
-//InputHandler* InputHandler::s_pInstance = 0;
-
-InputHandler::InputHandler() : m_mousePosition(0,0){ // i initialise this because it will be NULL if there is not mousemotion
-}
+#include "SDL2/SDL.h"
 
 bool InputHandler::getButtonState(int joy, int buttonNumber)
 {
-	if (joy < SDL_NumJoysticks()) {
+	if (joy < SDL_NumJoysticks())
 		return m_buttonStates[joy][buttonNumber];
-	}
 	return false;
 }
 
 void InputHandler::initialiseJoysticks()
 {
+	// initialize the three buttons to unpressed
 	for(int i = 0; i < 3; i++)
-    {
 		m_mouseButtonStates.push_back(false);
-    }
+	
 	if(SDL_WasInit(SDL_INIT_JOYSTICK) == 0)
-    {
 		SDL_InitSubSystem(SDL_INIT_JOYSTICK);
-    }
+	
 	if(SDL_NumJoysticks() > 0)
     {
 		for(int i = 0; i < SDL_NumJoysticks(); i++)
@@ -32,58 +27,36 @@ void InputHandler::initialiseJoysticks()
 			if(SDL_JoystickGetAttached(joy))
 			{
 				m_joysticks.push_back(joy);
-				m_joystickValues.push_back(std::make_pair
-										   (new Vector2D(0,0),new Vector2D(0,0))); // add our pair
+				m_joystickValues.push_back(
+					std::make_pair(new Vector2D(0,0),
+								   new Vector2D(0,0))); // add our pair
 				std::vector<bool> tempButtons;
-				for(int j = 0; j < SDL_JoystickNumButtons(joy); j++)
-				{
-					tempButtons.push_back(false);
-				}
+				for(int j = 0; j < SDL_JoystickNumButtons(joy); j++)								tempButtons.push_back(false);
 			}
 			else
-			{
-				std::cout << SDL_GetError();
-			}
+				std::cerr << SDL_GetError();
 		}
 		SDL_JoystickEventState(SDL_ENABLE);
 		m_bJoysticksInitialised = true;     
     }
 	else
-    {
 		m_bJoysticksInitialised = false;
-    }
-	std::cout << "Initialised "<< m_joysticks.size() << "joystick(s)" << std::endl;
+	std::cerr << "Initialised "<< m_joysticks.size() << "joystick(s)" << std::endl;
 }
 
 void InputHandler::clean()
 {
 	if(m_bJoysticksInitialised)
-    {
-		for(int i = 0; i < SDL_NumJoysticks(); i++)
-		{
-			SDL_JoystickClose(m_joysticks[i]);
-		}
-    }
+		for (SDL_Joystick* j : m_joysticks)
+			SDL_JoystickClose(j);
 }
 
-bool InputHandler::isKeyDown(SDL_Scancode key)
-{
-	if(m_keystates != 0)
-    {
-		if(m_keystates[key] == 1)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-    }
-	return false;
+bool InputHandler::isKeyDown(SDL_Scancode key) {
+	return (m_keystates && m_keystates[key] == 1);
 }
 
-void InputHandler::onKeyDown() {  
-}
+
+void InputHandler::onKeyDown() {}
 
 void InputHandler::onKeyUp() {}
 
@@ -97,109 +70,77 @@ void InputHandler::onMouseMove(SDL_Event& event) {
 
 void InputHandler::onMouseButtonDown(SDL_Event& event) {
 	if(event.type == SDL_MOUSEBUTTONDOWN)
-    {
-		if(event.button.button == SDL_BUTTON_LEFT)
-		{
+		switch (event.button.button) {
+		case SDL_BUTTON_LEFT: 
 			m_mouseButtonStates[LEFT] = true;
-		}
-		if(event.button.button == SDL_BUTTON_MIDDLE)
-		{
-			m_mouseButtonStates[MIDDLE] = true;
-		}
-		if(event.button.button == SDL_BUTTON_RIGHT)
-		{
+			break;
+		case SDL_BUTTON_RIGHT:
 			m_mouseButtonStates[RIGHT] = true;
+			break;
+		case SDL_BUTTON_MIDDLE:
+			m_mouseButtonStates[MIDDLE] = true;
+			break;
+		default:
+			break;
 		}
-    }
 }
 
 void InputHandler::onMouseButtonUp(SDL_Event& event) {
 	if(event.type == SDL_MOUSEBUTTONUP)
-    {
-		if(event.button.button == SDL_BUTTON_LEFT)
-		{
+		switch (event.button.button) {
+		case SDL_BUTTON_LEFT:
 			m_mouseButtonStates[LEFT] = false;
-		}
-		if(event.button.button == SDL_BUTTON_MIDDLE)
-		{
-			m_mouseButtonStates[MIDDLE] = false;
-		}
-		if(event.button.button == SDL_BUTTON_RIGHT)
-		{
+			break;
+		case SDL_BUTTON_RIGHT:
 			m_mouseButtonStates[RIGHT] = false;
+			break;
+		case SDL_BUTTON_MIDDLE:
+			m_mouseButtonStates[MIDDLE] = false;
+			break;
+		default:
+			break;
 		}
-    }
 }
 
 void InputHandler::onJoystickAxisMove(SDL_Event& event) {
 	if(event.type == SDL_JOYAXISMOTION) // check the type value
 	{
 		int whichOne = event.jaxis.which; // get which controller
-		// left stick move left or right
-		if(event.jaxis.axis == 0)
-	    {
+			
+		switch(event.jaxis.axis) {			
+		case 0:	// left stick move left or right		
 			if (event.jaxis.value > m_joystickDeadZone)
-			{
 				m_joystickValues[whichOne].first->setX(1);
-			}
 			else if(event.jaxis.value < -m_joystickDeadZone)
-			{
 				m_joystickValues[whichOne].first->setX(-1);
-			}
 			else
-			{
 				m_joystickValues[whichOne].first->setX(0);
-			}
-	    }
-		// left stick move up or down
-		if(event.jaxis.axis == 1)
-	    {
+			break;
+		case 1: // left stick move up or down
 			if (event.jaxis.value > m_joystickDeadZone)
-			{
 				m_joystickValues[whichOne].first->setY(1);
-			}
 			else if(event.jaxis.value < -m_joystickDeadZone)
-			{
 				m_joystickValues[whichOne].first->setY(-1);
-			}
 			else
-			{
 				m_joystickValues[whichOne].first->setY(0);
-			}
-	    }
-		// right stick move left or right
-		if(event.jaxis.axis == 3)
-	    {
+			break;
+		case 3: // right stick move left or right
 			if (event.jaxis.value > m_joystickDeadZone)
-			{
 				m_joystickValues[whichOne].second->setX(1);
-			}
 			else if(event.jaxis.value < -m_joystickDeadZone)
-			{
 				m_joystickValues[whichOne].second->setX(-1);
-			}
-	    }
-		else
-	    {
-			m_joystickValues[whichOne].second->setX(0);
-	    }
-
-		// right stick move up or down
-		if(event.jaxis.axis == 4)
-	    {
-			if (event.jaxis.value > m_joystickDeadZone)
-			{
-				m_joystickValues[whichOne].second->setY(1);
-			}
-			else if(event.jaxis.value < -m_joystickDeadZone)
-			{
-				m_joystickValues[whichOne].second->setY(-1);
-			}
 			else
-			{
+				m_joystickValues[whichOne].second->setX(0);
+			break;
+		case 4:	// right stick move up or down
+			if (event.jaxis.value > m_joystickDeadZone)
+				m_joystickValues[whichOne].second->setY(1);
+			else if(event.jaxis.value < -m_joystickDeadZone)
+				m_joystickValues[whichOne].second->setY(-1);
+			else
 				m_joystickValues[whichOne].second->setY(0);
-			}
-	    }
+			break;
+		}
 	}
 }
 
@@ -224,7 +165,6 @@ void InputHandler::update()
 	m_keystates = (Uint8*) SDL_GetKeyboardState(0);
 	SDL_Event event;
 	while(SDL_PollEvent(&event))
-    {
 		switch (event.type)
 		{
 		case SDL_QUIT:
@@ -257,7 +197,6 @@ void InputHandler::update()
 		default:
 			break;
 		}
-    }
 }
 
 int InputHandler::xvalue(int joy, int stick)
@@ -265,13 +204,9 @@ int InputHandler::xvalue(int joy, int stick)
 	if(m_joystickValues.size() > 0)
     {
 		if(stick == 1)
-		{
 			return m_joystickValues[joy].first->getX();
-		}
 		else if(stick == 2)
-		{
 			return m_joystickValues[joy].second->getX();
-		}
     }
 	return 0;
 }
@@ -280,13 +215,9 @@ int InputHandler::yvalue(int joy, int stick)
 	if(m_joystickValues.size() > 0)
     {
 		if(stick == 1)
-		{
 			return m_joystickValues[joy].first->getY();
-		}
 		else if(stick == 2)
-		{
 			return m_joystickValues[joy].second->getY();
-		}
     }
 	return 0;
 }
--- src/InputHandler.h
+++ src/InputHandler.h
@@ -1,76 +1,120 @@
 #ifndef __INPUTHANDLER__
 #define __INPUTHANDLER__
 
-#include "SDL2/SDL.h"
 #include <vector>
-#include <iostream>
 #include "Vector2D.h"
 #include <utility>
 #include "Singleton.h"
+#include "SDL2/SDL.h"
 
+/**
+ * A value for each mouse's buttons.
+ */
 enum mouse_buttons
-  {
+{
     LEFT = 0,
     MIDDLE = 1,
     RIGHT = 2
-  };
+};
 
-
+/**
+ * Handle all events made by user (mouse, keyboard, joystick)
+ */
 class InputHandler : public Singleton<InputHandler>
 {
 	friend class Singleton; // to access private constructor
+	
 public:
-  const int m_joystickDeadZone = 10000;
-	/*static InputHandler* Instance()
-  {
-    if(s_pInstance == 0)
-      {
-	s_pInstance = new InputHandler();
-      }
-    return s_pInstance;
-	};*/
-  void update();
-  void clean();
-  void initialiseJoysticks();
-  bool joysticksInitialised() {
-    return m_bJoysticksInitialised; };
-  std::vector<std::pair<Vector2D*, Vector2D*>> m_joystickValues;
-  int xvalue(int joy, int stick);
-  int yvalue(int joy, int stick);
-  std::vector<std::vector<bool>> m_buttonStates;
-  bool getButtonState(int joy, int buttonNumber);
-  std::vector<bool> m_mouseButtonStates;
-  bool getMouseButtonState(int buttonNumber)
-  {
-    return m_mouseButtonStates[buttonNumber];
-  }
-  Vector2D* getMousePosition()
-  {
-    return &m_mousePosition;
-  }
-  Uint8* m_keystates;
-  bool isKeyDown(SDL_Scancode key);
-  void reset();
-private:
-  Vector2D m_mousePosition;
-  InputHandler();
-  ~InputHandler() {};
-	//static InputHandler* s_pInstance;
-  std::vector<SDL_Joystick*> m_joysticks;
-  bool m_bJoysticksInitialised;
+	const int m_joystickDeadZone = 10000;
 
-  // private functions to handle different event types
-  // handle keyboard events
-  void onKeyDown();
-  void onKeyUp();
-  // handle mouse events
-  void onMouseMove(SDL_Event& event);
-  void onMouseButtonDown(SDL_Event& event);
-  void onMouseButtonUp(SDL_Event& event);
-  // handle joysticks events
-  void onJoystickAxisMove(SDL_Event& event);
-  void onJoystickButtonDown(SDL_Event& event);
-  void onJoystickButtonUp(SDL_Event& event);
+/**
+ * Handle all events sent by SDL.
+ */
+	void update();
+	
+	/**
+	 * Clean the class by close joysticks
+	 */
+	void clean();	
+	void initialiseJoysticks();
+	bool joysticksInitialised() { return m_bJoysticksInitialised; }
+
+	/**
+	 * Contains for each joystick a pair of Vector2D
+	 * which represent the movement of both
+	 * analog stick of the joystick.
+	 */
+	std::vector<std::pair<Vector2D*, Vector2D*>> m_joystickValues;
+	
+	/**
+	 * Get the x value of joystick'stick given.
+	 */
+	int xvalue(int joy, int stick);
+
+	/**
+	 * Get the y value of joystick'stick given.
+	 */
+	int yvalue(int joy, int stick);
+
+	/**
+	 * Each vector contained,
+	 * contains a boolean value for each joystick's button.
+	 * The boolean is true if the button is pressed.
+	 */
+	std::vector<std::vector<bool>> m_buttonStates;
+	
+	/**
+	 * Return true if the "buttonNumber" button of "joy" is pressed.
+	 */
+	bool getButtonState(int joy, int buttonNumber);
+
+	/**
+	 * Contains a boolean for each mouse's button.
+	 * The boolean is true is the button is pressed.
+	 */
+	std::vector<bool> m_mouseButtonStates;
+
+	/**
+	 * Return true if the "buttonNumber" mouse's button is pressed.
+	 */
+	bool getMouseButtonState(int buttonNumber)
+		{
+			return m_mouseButtonStates[buttonNumber];
+		}
+	
+	Vector2D* getMousePosition()
+		{
+			return &m_mousePosition;
+		}
+	
+	/**
+	 * Contains the state of all keyboard's keys.
+	 */
+	Uint8* m_keystates;
+	bool isKeyDown(SDL_Scancode key);
+	/**
+	 * Reset all mouse's buttons to non-pressed (false).
+	 */
+	void reset();
+
+private:
+	InputHandler(){};
+	Vector2D m_mousePosition;
+	std::vector<SDL_Joystick*> m_joysticks;
+	bool m_bJoysticksInitialised;
+
+	// private functions to handle different event types
+	// handle keyboard events
+	void onKeyDown();
+	void onKeyUp();
+	// handle mouse events
+	void onMouseMove(SDL_Event& event);
+	void onMouseButtonDown(SDL_Event& event);
+	void onMouseButtonUp(SDL_Event& event);
+	// handle joysticks events
+	void onJoystickAxisMove(SDL_Event& event);
+	void onJoystickButtonDown(SDL_Event& event);
+	void onJoystickButtonUp(SDL_Event& event);
 };
 typedef InputHandler TheInputHandler;
 
