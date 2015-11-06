80658b7e51e496ecf7f9e2265ab7a183c122ab75
Darenn Keller <keller.darenn@gmail.com>
2015-11-06 14:41:38 +0100

    Optimize and doc TextureManager class

--- src/TextureManager.h
+++ src/TextureManager.h
@@ -5,34 +5,45 @@
 #include <map>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include "Singleton.h"
 
-class TextureManager 
+class TextureManager  : public Singleton<TextureManager>
 {
- public:  
+	friend Singleton;
+	
+public:  
+	~TextureManager() {};
 
-  ~TextureManager() {}; 
-  bool load(std::string fileName, std::string id,
-	    SDL_Renderer* pRenderer);
-  void draw(std::string id, int x, int y, int width, int
-	    height, SDL_Renderer* pRenderer, 
-	    SDL_RendererFlip flip = SDL_FLIP_NONE);
-  void drawFrame(std::string id, int x, int y, int width, int
-		 height, int currentRow, int currentFrame, SDL_Renderer*
-		 pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
-  std::map<std::string, SDL_Texture*> m_textureMap; 
-  static TextureManager* Instance()
-  {
-    if(TextureManager::s_pInstance == 0)
-      {
-	TextureManager::s_pInstance = new TextureManager();
-	return TextureManager::s_pInstance;
-      }
-    return TextureManager::s_pInstance;
-  }
-  void clearFromTextureMap(std::string id);
- private:
-  TextureManager() {};
-  static TextureManager* s_pInstance;  
+/**
+ * Load the image "fileName", and store it in "m_textureMap"
+ * "pRenderer" : the renderer of the frame
+ */
+	bool load(std::string fileName, std::string id,
+			  SDL_Renderer* pRenderer);
+
+	/**
+	 * Draw the texture "id" on position "x" and "y"
+	 */
+	void draw(std::string id, int x, int y, int width, int
+			  height, SDL_Renderer* pRenderer, 
+			  SDL_RendererFlip flip = SDL_FLIP_NONE);
+
+/**
+ * Use that for draw a sprite.
+ * "width" : the width of one sprite's bloc
+ * "heigth" : the heigth of one sprite's bloc
+ * "currentFrame" : the bloc to draw
+ */
+	void drawFrame(std::string id, int x, int y, int width, int
+				   height, int currentRow, int currentFrame, SDL_Renderer*
+				   pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
+
+	// contains all textures used by the game
+	std::map<std::string, SDL_Texture*> m_textureMap;	
+	void clearFromTextureMap(std::string id);
+
+private:
+	TextureManager() {}
 
 };
 
